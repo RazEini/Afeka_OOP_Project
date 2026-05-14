3ba2ef422d5179ea7012faaf9d8c2b861f0a20ef
Raz Yakoby <razrazraz189@gmail.com>
2026-05-14 15:17:55 +0300

    create class Lecturer

---
+++ Afeka-workNum1- OOP/src/classes/Lecturer.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Lecturer {
+    public String name;
+    public String id;
+    public enum  degree {
+       bachelor_degree , master_degree, DR, Professor
+    }
+    public String degreeName;
+    public Department department;
+
+}
