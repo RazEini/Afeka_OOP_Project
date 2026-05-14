8c35cf31b9a7e5277f215a0fccb13f7a68b31106
Raz Yakoby <razrazraz189@gmail.com>
2026-05-14 16:43:23 +0300

    create class Committe and add set and get in class Lecturer

---
+++ Afeka-workNum1- OOP/src/classes/Committe.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Committe {
+    public String name;
+    public Lecturer[] lecturer;
+    //public Lecturer ; יו"ר הוועדה
+}
--- Afeka-workNum1- OOP/src/classes/Lecturer.java
+++ Afeka-workNum1- OOP/src/classes/Lecturer.java
@@ -9,4 +9,44 @@ public class Lecturer {
     public String degreeName;
     public Department department;
 
+    public Lecturer(){
+        this.name = "";
+        this.id = "";
+        this.degreeName = "";
+        department = new Department();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDegree(String degree) {
+        this.degreeName = degree;
+    }
+
+    public void SetDepartment(Department department){
+        this.department = department;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getDegreeName(){
+        return this.degreeName;
+    }
+
+    public Department getDepartment(){
+        return this.department;
+    }
+
+
+
 }
