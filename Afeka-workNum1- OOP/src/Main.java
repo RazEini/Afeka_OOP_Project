354ace0a208f13bd959f69a617fa75a29278bb87
Raz Yakoby <razrazraz189@gmail.com>
2026-05-15 15:02:39 +0300

    fixed the main

--- Afeka-workNum1- OOP/src/Main.java
+++ Afeka-workNum1- OOP/src/Main.java
@@ -10,14 +10,10 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Administrative administrative = new Administrative();
-        administrative.getCollegeName();
+        System.out.print("Welcome! Enter the name of the college: ");
+        String collegeName = scanner.nextLine();
+        Administrative administrative = new Administrative(collegeName);
 
-        String[] lecturers = new String[1];
-        String[] committees = new String[1];
-        String[] departments = new String[1];
-
-        int lecCount = 0, comCount = 0, depCount = 0;
         int choice = -1;
 
         while (choice != 0) {
@@ -30,6 +26,9 @@ public class Main {
             System.out.println("6 - Display average salary of committee lecturers");
             System.out.println("7 - Display all Lecturers");
             System.out.println("8 - Display all Committees");
+            System.out.println("9 - Display average salary of all committees");
+            System.out.println("10 - Display average salary of committee lecturers");
+            System.out.println("11 - Display average salary of committee lecturers");
             System.out.println("0 - Exit");
             System.out.print("Select an option: ");
 
@@ -44,29 +43,25 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    lecturers = addItem(lecturers, lecCount, "Lecturer");
-                    lecCount++;
                     break;
                 case 2:
-                    committees = addItem(committees, comCount, "Committee");
-                    comCount++;
                     break;
                 case 3:
-                    departments = addItem(departments, depCount, "Department");
-                    depCount++;
                     break;
                 case 4:
-                    placementLecturersToCommittee(lecturers, committees);
                     break;
                 case 5:
                 case 6:
-                    System.out.println("Functionality not yet implemented.");
                     break;
                 case 7:
-                    displayItems(lecturers, lecCount, "Lecturer");
                     break;
                 case 8:
-                    displayItems(committees, comCount, "Committee");
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
                     break;
                 case 0:
                     System.out.println("Exiting the program...");
