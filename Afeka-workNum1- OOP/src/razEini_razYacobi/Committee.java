d75b123ebd4d6a72de7c9c6261561f36950160dc
Raz Yakoby <razrazraz189@gmail.com>
2026-05-27 10:34:42 +0300

    remove getLecturers function, never used

--- Afeka-workNum1- OOP/src/razEini_razYacobi/Committee.java
+++ Afeka-workNum1- OOP/src/razEini_razYacobi/Committee.java
@@ -26,17 +26,6 @@ public class Committee {
         }
     }
 
-    public Lecturer[] getLecturers() {
-        if (this.lecturers_Array == null) return new Lecturer[1];
-        Lecturer[] copy = new Lecturer[this.lecturers_Array.length];
-        for (int i = 0; i < this.lecturers_Array.length; i++) {
-            if (this.lecturers_Array[i] != null) {
-                copy[i] = new Lecturer(this.lecturers_Array[i]);
-            }
-        }
-        return copy;
-    }
-
     public Lecturer getChairman() {
         return this.chairman;
     }
